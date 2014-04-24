66479eb0365adcfc2c4e0c2e4bb8170748d17a4a
wmcatadmin <wmcatd@wmcatadmins-MacBook-Pro.local>
2014-04-24 18:36:06 -0400

    Initial Commit

---
+++ Hello World CoLearning/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World CoLearning
+//
+//  Created by wmcatadmin on 4/24/14.
+//  Copyright (c) 2014 WMCAT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
