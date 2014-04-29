26cff74e0d6a0d983acc4e2e46a0b80ae3f050f6
wmcatadmin <wmcatd@wmcatmini09.wmcat.org>
2014-04-29 18:48:08 -0400

    Commit for day one of class

--- Hello World CoLearning/main.m
+++ Hello World CoLearning/main.m
@@ -17,15 +17,23 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         int f = getIntegerFromConsole(@"Enter the value:");
+        //float decimal = 2.91543;
+        NSString *string = @"this is a string.";
         
         if (f <= 10) {
-            NSLog (@"This is a small number");
+            //NSLog (@"This is a small number");
+            string = @"small number";
+            NSLog (@"This is a %@.", string);
         }
         else if (f >= 100) {
-            NSLog (@"This is a large number");
+            //NSLog (@"This is a large number");
+            string = @"large number";
+            NSLog (@"This is a %@.", string);
         }
         else if (f > 10 && f < 100) {
-            NSLog (@"This is a medium number");
+            //NSLog (@"This is a medium number");
+            string = @"medium number";
+            NSLog (@"This is a %@.", string);
         }
     }
     return 0;
